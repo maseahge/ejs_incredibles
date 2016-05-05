917cafde258cbd6e197c5b42903e14316849b2a6
la-mari <karima19@gmail.com>
2016-05-04 19:18:53 -0700

    added username input for chat

--- public/javascripts/sketch.js
+++ public/javascripts/sketch.js
@@ -34,19 +34,27 @@ socket.on('newBolita', function(data){
 var button = document.querySelector('#send-message');
 button.addEventListener('click', function(event){
 	var input = document.querySelector('#message');
+	var input_author = document.querySelector('#author');
 	console.log(input.value);
+	console.log(input_author.value);
 	//how do I append the message to the ul with id=messages?
 	socket.emit('chat', input.value);
+	socket.emit('chat', input_author.value);
 	input.value = '';
+	input_author.value = '';
 })
 
 
 socket.on('message-received', function(data){
 	console.log(data);
 	var newMessage = document.createElement('div');
+	var newMessageAuthor = document.createElement('div');
 	newMessage.className = "messages";
+	newMessageAuthor.className = "authors"
 	newMessage.appendChild(document.createTextNode(data));
+	newMessageAuthor.appendChild(document.createTextNode(data));
 	console.log(newMessage)
+	console.log(newMessageAuthor)
 	console.log(document.querySelector('#messages'))
 	document.querySelector('#messages').appendChild(newMessage);
 });
