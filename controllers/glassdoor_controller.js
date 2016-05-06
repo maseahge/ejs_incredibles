522de6d5a8bb5e542daab7c066c70c47501e34a7
la-mari <karima19@gmail.com>
2016-05-05 20:41:47 -0700

    logging json data in console

--- controllers/glassdoor_controller.js
+++ controllers/glassdoor_controller.js
@@ -2,25 +2,6 @@
 var glassdoor = require('../models/glassdoor')
 var baseUrl = "http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p="+GLASSDOOR_PARTNER_ID+"&t.k="+GLASSDOOR_CLIENT_SECRECT+"&action=employers"
 
-function searchGiphy(query){
-
-
-	$.ajax({
-	  url: 'http://api.giphy.com/v1/gifs/search?q=' + query + '&api_key=&api_key=dc6zaTOxFJmzC',
-	  success: function(data){
-	  	console.log(data)
-	  	// $('body').append('' + data)
-	  	$('#results').html('<img src=' + data.data[0].images.fixed_height.url + '>')
-	  	},
-	  error: function(response){
-	  	console.log('error time')
-	  }
-	 })
-};
-
-$('#searchBtn').click(function(){
-	searchGiphy($('#searchBar').val())
-})
 
 
 
--- routes/gdroutes.js
+++ routes/gdroutes.js
@@ -4,12 +4,22 @@ var glassdoor = require('../models/glassdoor');
 
 router.get('/', function(req, res){
   // var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  // console.log(req.query)
+  // // { company: 'facebook', location: 'illinois' }
+  // var ip = '127.0.0.1' // this should change on heroku to outside ip addresses
+  // glassdoor(ip, req.headers['user-agent'], req.query.company, req.query.location, function(results){
+    res.render('glassdoor');
+  // });
+});
+
+router.get('/json', function(req, res){
+  // var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   console.log(req.query)
+  // { company: 'facebook', location: 'illinois' }
   var ip = '127.0.0.1' // this should change on heroku to outside ip addresses
   glassdoor(ip, req.headers['user-agent'], req.query.company, req.query.location, function(results){
-    res.render('glassdoor', {results: results});
+    res.send(results);
   });
 });
 
-// query string http://localhost:3000/glassdoor?company=facebook&location=illinois
 module.exports= router;
