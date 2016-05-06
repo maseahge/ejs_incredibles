99ee141d0ca09ce2cc844dc3c9c9e8d8ee894b65
la-mari <karima19@gmail.com>
2016-05-05 17:20:46 -0700

    gd updates

--- controllers/glassdoor_controller.js
+++ controllers/glassdoor_controller.js
@@ -1,3 +1,21 @@
 var glassdoor = require('../models/glassdoor')
 
 
+
+// var button = $('#clicker');
+// var companySearch = $('#companySearch');
+// var location = $('#location');
+
+// button.on('click', function(event){
+// 	var searchEmp = companySearch.val()
+//   	var searchLoc = location.val()
+// });
+
+// var searchGlassdoor = function(){
+//  $('#clicker').on('click', function(event){
+//    var searchEmp = companySearch.val(),
+//    var searchLoc = location.val()}),
+//    res.redirect('baseUrl + "&userip=" + userIP + "&useragent=" + encodeURIComponent(userAgent) + "&q=" + searchEmp + "&l=" + searchLoc;')
+//  }
+
+
