89e75a7aa0ff19d7085647f02781b6d5d2669a12
la-mari <karima19@gmail.com>
2016-05-05 19:40:16 -0700

    update

--- routes/gdroutes.js
+++ routes/gdroutes.js
@@ -4,11 +4,12 @@ var glassdoor = require('../models/glassdoor');
 
 router.get('/', function(req, res){
   // var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log(req.query)
   var ip = '127.0.0.1' // this should change on heroku to outside ip addresses
-  glassdoor(ip, req.headers['user-agent'], 'google', 'california', function(results){
+  glassdoor(ip, req.headers['user-agent'], req.query.company, req.query.location, function(results){
     res.render('glassdoor', {results: results});
   });
 });
 
-
+// query string http://localhost:3000/glassdoor?company=facebook&location=illinois
 module.exports= router;
